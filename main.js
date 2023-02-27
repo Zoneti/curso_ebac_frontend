6a4711548dcb4ac7e023231ff63602a3ff13f6b4
Gabriel Zoneti <gabriel.zoneti@gmail.com>
2023-02-27 18:49:07 -0300

    Resolução Tarefa Mod09

---
+++ main.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+
+    $('form').on('submit', function(e){
+        e.preventDefault();
+        const tarefa = $('#nome-tarefa').val();
+        const nTask = $(`<li style="display:none">${tarefa}</li>`);
+        $(nTask).appendTo('ol');
+        $(nTask).fadeIn(1000);
+        //$('#nome-tarefa').val('');
+    })
+
+    $('#lista-tarefas').on('click', 'li', function(e){
+        
+        var v_css = $(e.target).css('text-decoration');
+        
+        if( v_css == 'line-through solid rgb(0, 0, 0)'){
+            $(e.target).css('text-decoration','none');
+        }else{
+            $(e.target).css('text-decoration','line-through');
+        }
+        
+    })
+})
